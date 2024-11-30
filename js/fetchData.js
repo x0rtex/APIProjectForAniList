ce480467b517f75634f2a70ef8b81c5f5d946b91
Alekss <x0rtex420@gmail.com>
2024-11-30 20:10:57 +0000

    Added sample code to fetch data from AniList. Added Grunt to watch codebase and concat/minify js files into one script.min.js file.

---
+++ js/fetchData.js
@@ -0,0 +1,42 @@
+﻿// Define query from .gql file
+const query = fetch('query.gql')
+    .then(response => response.text())
+
+// Define our query variables and values that will be used in the query request
+const variables = {
+    id: 15125
+};
+
+// Define the config we'll need for our Api request
+const url = 'https://graphql.anilist.co',
+    options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            query: query,
+            variables: variables
+        })
+    };
+
+// Make the HTTP Api request
+fetch(url, options).then(handleResponse)
+    .then(handleData)
+    .catch(handleError);
+
+function handleResponse(response) {
+    return response.json().then(function (json) {
+        return response.ok ? json : Promise.reject(json);
+    });
+}
+
+function handleData(data) {
+    console.log(data);
+}
+
+function handleError(error) {
+    alert('Error, check console');
+    console.error(error);
+}
