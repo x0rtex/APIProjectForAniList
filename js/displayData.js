f8ca9f81ef15c9a87dd0afcc7062d7104810794d
Alekss <x0rtex420@gmail.com>
2024-12-08 19:15:11 +0000

    All media data is now displayed on the page. Extracted logic into different functions.

--- js/displayData.js
+++ js/displayData.js
@@ -3,6 +3,8 @@ export function displayData(jsonData) {
     const user = data.user;
     const lists = data.lists;
 
+    console.log(data);
+
     const element = document.getElementById("user");
     element.innerHTML = '';
 
@@ -12,8 +14,34 @@ export function displayData(jsonData) {
     element.appendChild(userName);
 
     lists.forEach((list) => {
-        const listItem = document.createElement("li");
-        listItem.textContent = list.name;
+        const listItem = createListItem(list);
+        listItem.innerHTML += list.entries.map(createMediaElement).join('');
         element.appendChild(listItem);
     })
+}
+
+function createListItem(list) {
+    const listItem = document.createElement("li");
+    listItem.setAttribute("class", "list");
+    const title = document.createElement("h2");
+    title.setAttribute("class", "listTitle");
+    title.textContent = `${list.name} (${list.entries.length})`;
+    listItem.appendChild(title);
+    return listItem;
+}
+
+function createMediaElement(media) {
+    return `
+    <article class="media">
+      <img class="mediaImage" src="${media.media.coverImage.extraLarge}" alt="Cover Image">
+      <h3 class="mediaTitle">${media.media.title.english || media.media.title.romaji || media.media.title.native} <span class="score">${media.media.averageScore}%</span></h3>
+      <h4 class="mediaTags">${formatMediaTags(media)}</h4>
+      <hr class="divider" />
+      <p class="mediaDescription">${media.media.description}</p>
+    </article>
+  `;
+}
+
+function formatMediaTags(media) {
+    return `${media.media.season} | ${media.media.seasonYear} | ${media.media.genres.join(' | ')}`;
 }
